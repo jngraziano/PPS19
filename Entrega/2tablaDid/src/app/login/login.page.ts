9f0df1814cfd30f72daa5cd699815cb0b4bbcac4
jngraziano <jngraziano@gmail.com>
2019-04-11 03:49:39 -0300

    2tabladic_loginOK

--- Entrega/2tablaDid/src/app/login/login.page.ts
+++ Entrega/2tablaDid/src/app/login/login.page.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
 import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
-import { AlertController } from '@ionic/angular';
+import { AlertController, 
+        ActionSheetController, 
+        ToastController   } from '@ionic/angular';
+
 
 
 
@@ -31,61 +34,123 @@ export class LoginPage implements OnInit {
 
 
   constructor(private firebaseAuthentication: FirebaseAuthentication, private auth: AuthService, private router: Router,
-    public alertController: AlertController) { }
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public actionSheetController: ActionSheetController
+    ) { }
 
 ngOnInit() {
 }
 
+async creoToast(rta: boolean) {
+
+  if(rta == true)
+  {
+    const toast = await this.toastController.create({
+      message: 'Autenticación exitosa.',
+      color: 'danger',
+      showCloseButton: false,
+      position: 'bottom',
+      closeButtonText: 'Done',
+      duration: 2000 
+    });
+
+    toast.present();
+
+
+  }
+  else{
+    const toast = await this.toastController.create({
+      message: 'Usuario/contraseña incorrectos.',
+      color: 'danger',
+      showCloseButton: false,
+      position: 'top',
+      closeButtonText: 'Done',
+      duration: 2000 
+    });
+
+    toast.present();
+
+  }
+ 
+    
+}
+
+
+async creoSheet() {
+  const actionSheet = await this.actionSheetController.create({
+    header: 'Ingresar como ...',
+    cssClass: 'actSheet',
+      buttons: [{
+      text: 'admin',
+      icon: 'body',
+      handler: () => {
+        
+        this.username = "admin@gmail.com";
+        this.password= "admin1111";
+
+      }
+    }, {
+      text: 'invitado',
+      icon: 'body',
+      handler: () => {
+        this.username = "invitado@gmail.com";
+        this.password= "invitado2222";
+      }
+    }, {
+      text: 'usuario',
+      icon: 'body',
+      handler: () => {
+        this.username = "usuario@gmail.com";
+        this.password= "usuario3333";
+      }
+    }, {
+      text: 'anonimo',
+      icon: 'body',
+      handler: () => {
+        this.username = "anonimo@gmail.com";
+        this.password= "anonimo4444";
+      }
+    },{
+      text: 'tester',
+      icon: 'body',
+      handler: () => {
+        this.username = "tester@gmail.com";
+        this.password= "tester5555";
+      }
+    }, {
+      text: 'Cancelar',
+      icon: 'close',
+      cssClass: 'btnCancel',
+      role: 'cancel',
+      handler: () => {
+       
+      }
+    }]
+  });
+  await actionSheet.present();
+}
+
+
+
+
+
 login()
 {
 
-// this.auth.loginUser(this.user.email,this.user.password ).then((user) => {
-this.auth.loginUser(this.username,this.password ).then((user) => {
-if(this.username == "" || this.password == "")
-{
-this.presentAlert(false);  
+  this.auth.loginUser(this.username,this.password ).then((user) => {
+    // if(this.username == "" || this.password == "")
+    // {
+    //   this.creoToast(false);  
+    // }
+  this.creoToast(true);  
+  this.router.navigateByUrl('/tabs'); 
+  }
+  ) 
+  .catch(err=>{
+    
+    this.creoToast(false);  
+  });
 }
-this.presentAlert(true);  
-this.router.navigateByUrl('/tabs'); 
-}
-) 
-.catch(err=>{
-
-this.presentAlert(false);  
-});
-}
-
-async presentAlert(estado: boolean) {
-
-if(estado == true)
-{
-const alert = await this.alertController.create({
-header: 'Bienvenido.',
-subHeader: '',
-message: 'Autenticación exitosa.',
-buttons: ['OK']
-});
-
-await alert.present();
-
-
-}
-else{
-const alert = await this.alertController.create({
-header: 'Error',
-subHeader: '',
-message: 'Usuario/Contraseña incorrectos.',
-buttons: ['OK']
-});
-
-await alert.present();
-
-}
-
-
-}
-
-
-
 
 }
